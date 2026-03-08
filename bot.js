6934bd0e4d78e2e5aa0ca0c29d667963432d90a3
lynussxs <melanie74731@gmail.com>
2026-03-08 17:14:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,25 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot ready: ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN);
